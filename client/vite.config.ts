ece3edbfc8e22c79c3f67ad4779014fbb7bb3199
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-12 22:34:57 +0300

    chore(vite): update server port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()]
+  plugins: [vue(), vueDevTools()],
+  server: {
+    port: 3000
+  },
 });
