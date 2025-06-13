b18e4eb5d05a1110d1e1e64098938fbf3ca8dba5
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-13 10:14:19 +0300

    fix(supabase): remove options

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -3,10 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-    detectSessionInUrl: false
-  }
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
