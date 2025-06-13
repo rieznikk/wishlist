92c73c2d383957652a55a77016133d5bb2bb2291
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-13 08:45:36 +0300

    feat(Button): add component

---
+++ client/src/components/ui/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :disabled="loading">
+    <slot v-if="!loading" />
+    <span v-else class="button-spinner"></span>
+  </button>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const props = defineProps({ loading: Boolean });
+  const loading = computed(() => props.loading);
+</script>
