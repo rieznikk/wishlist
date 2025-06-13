84f23026a029122b6bb5147fec4c6397f2b817c0
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-13 10:14:38 +0300

    feat(router): add security guard

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,13 @@
+import type { NavigationGuardNext, RouteLocationNormalized, RouteLocationNormalizedLoaded } from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
+import { getUser } from '../composables/useAuth';
+
 import Home from '../views/HelloWorld.vue';
 import AuthView from '../views/AuthView.vue';
 import SigninForm from '../components/auth/SigninForm.vue';
 import SignupForm from '../components/auth/SignupForm.vue';
 import ResetPasswordForm from '../components/auth/ResetPasswordForm.vue';
+import UpdatePasswordForm from '../components/auth/UpdatePasswordForm.vue';
 
 const routes = [
   {
@@ -19,6 +23,21 @@ const routes = [
       { path: 'signin', name: 'Signin', component: SigninForm },
       { path: 'signup', name: 'Signup', component: SignupForm },
       { path: 'reset', name: 'Reset', component: ResetPasswordForm },
+      { 
+        path: 'update-password', 
+        name: 'UpdatePassword', 
+        component: UpdatePasswordForm,
+        beforeEnter: async (_to: RouteLocationNormalized, _from: RouteLocationNormalizedLoaded, next: NavigationGuardNext) => {
+          const user = await getUser();
+
+          if (!user) {
+            next({ name: 'Signin' });
+            return;
+          }
+
+          next();
+        }
+      }
     ]
   }
 ];
