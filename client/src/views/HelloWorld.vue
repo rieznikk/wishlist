640a60d22c3d207dc77bcae2b8224f89981ee2e1
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-14 22:55:52 +0300

    feat(home): test home page

--- client/src/views/HelloWorld.vue
+++ client/src/views/HelloWorld.vue
@@ -1,41 +1,22 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <Button @click="handleLogout" :loading="isLoading">Logout</Button>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { useAuthStore } from '../stores/auth';
+  import Button from '../components/ui/Button.vue';
+  import { logout } from '../composables/useAuth';
+  import { useRouter } from 'vue-router';
+
+  const auth = useAuthStore();
+  const router = useRouter();
+  const isLoading = ref(false);
+  
+  const handleLogout = async () => {
+    isLoading.value = true;
+    await logout();
+    await auth.fetchUser();
+    router.push({ name: 'Signin' });
+  }
+</script>
