ad50b2a0306fea97d53cd915762a0ceb1790673d
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-15 21:02:03 +0300

    feat(LocaleSelector): init

--- client/src/components/layout/AppHeader.vue
+++ client/src/components/layout/AppHeader.vue
@@ -1,11 +1,12 @@
 <template>
   <header>
+    <LocaleSelector />
     <ThemeToggle />
-    <!-- locale selector (UA/EN) -->
     <!-- account (image, popup + settings + logout/signin) -->
   </header>
 </template>
 
 <script setup lang="ts">
   import ThemeToggle from '../ui/ThemeToggle.vue';
+  import LocaleSelector from '../ui/LocaleSelector.vue';
 </script>
