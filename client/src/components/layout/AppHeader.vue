f71f1c4adef60a8ff5a048229f834ef78b7491f4
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-15 19:20:39 +0300

    feat(ThemeToggle): add

---
+++ client/src/components/layout/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <ThemeToggle />
+    <!-- locale selector (UA/EN) -->
+    <!-- account (image, popup + settings + logout/signin) -->
+  </header>
+</template>
+
+<script setup lang="ts">
+  import ThemeToggle from '../ui/ThemeToggle.vue';
+</script>
