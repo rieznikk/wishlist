4831dde7c4faa677eefb5cf3091b65e3771096db
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-23 21:06:54 +0300

    feat(AccountIcon): add component

--- client/src/components/layout/AppHeader.vue
+++ client/src/components/layout/AppHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <header>
+    <AccountIcon v-if="isLoggedIn" />
     <LocaleSelector />
     <ThemeToggle />
-    <!-- account (image, popup + settings + logout/signin) -->
   </header>
 </template>
 
 <script setup lang="ts">
   import ThemeToggle from '../ui/ThemeToggle.vue';
   import LocaleSelector from '../ui/LocaleSelector.vue';
+  import AccountIcon from '../ui/AccountIcon.vue';
+  import { useAuthStore } from '../../stores/auth';
+  import { computed } from 'vue';
+
+  const auth = useAuthStore();
+  const isLoggedIn = computed(() => auth.isLoggedIn);
 </script>
