7da78b003357a195f4cc59203fcbb92adda9f669
rieznikk <oleksandr.rieznikk@gmail.com>
2025-06-24 10:39:29 +0300

    feat(AccountSettings): init

---
+++ client/src/components/account/AccountSettings.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <GoBack page="Wishlists">{{ $t('components.go_back.back_to_home') }}</GoBack>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import GoBack from '../ui/GoBack.vue';
+</script>
