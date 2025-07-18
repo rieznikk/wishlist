2f2c6eaf81068c093aaca32b485a0254865cc0f1
rieznikk <oleksandr.rieznikk@gmail.com>
2025-07-18 10:16:12 +0300

    types(wishlists): add WishlistCreateInput

--- client/src/types/wishlists.ts
+++ client/src/types/wishlists.ts
@@ -1,13 +1,19 @@
 interface Wishlist {
   name: string
   owner_id: string,
-  id?: string,
+  id: string,
+  is_public: boolean
+  created_at: string
   description?: string
-  is_public?: boolean
   magic_token?: string
-  created_at?: string
 };
 
+interface WishlistCreateInput {
+  name: string;
+  owner_id: string;
+}
+
 export type {
-  Wishlist
+  Wishlist,
+  WishlistCreateInput
 };
